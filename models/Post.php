733597a3dc65bae7330dced697ebf5232d83942c
Kavishka Fernando <kavishkafernando64@gmail.com>
2022-07-20 23:46:15 +0530

    2nd

--- models/Post.php
+++ models/Post.php
@@ -34,4 +34,59 @@ public function __construct($db)
        $stmt-> execute();
        return $stmt;
  }
+ //Get Single post
+ public function read_single(){
+//create query
+$query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+FROM ' . $this->table . ' p
+LEFT JOIN
+  categories c ON p.category_id = c.id
+WHERE
+ p.id = ?
+ LIMIT 0,1';
+ //prepare statement
+ $stmt = $this->conn->prepare($query);
+ //BIND ID
+ $stmt->bindParam(1,$this->id);
+ //Execute query
+ $stmt->execute();
+ $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ //set properties
+ $this->title = $row['title'];
+ $this->body = $row['body'];
+ $this->author = $row['author'];
+ $this->category_id = $row['category_id'];
+ $this->category_name = $row['category_name'];
+
+ }
+ //create post
+ public function create(){
+   // Create query
+   $query = 'INSERT INTO ' . $this->table . ' SET title = :title, body = :body, author = :author, category_id = :category_id';
+
+   // Prepare statement
+   $stmt = $this->conn->prepare($query);
+
+   // Clean data
+   $this->title = htmlspecialchars(strip_tags($this->title));
+   $this->body = htmlspecialchars(strip_tags($this->body));
+   $this->author = htmlspecialchars(strip_tags($this->author));
+   $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+    
+    // Bind data
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':body', $this->body);
+    $stmt->bindParam(':author', $this->author);
+    $stmt->bindParam(':category_id', $this->category_id);
+
+    // Execute query
+    if($stmt->execute()) {
+      return true;
 }
+
+// Print error if something goes wrong
+printf("Error: %s.\n", $stmt->error);
+
+return false;
+}
+ }
